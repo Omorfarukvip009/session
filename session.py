e19e47ec052944ab8c3925ca768e2c9f66cdecfc
Omorfarukvip009 <mayemislam24@gmail.com>
2025-09-04 10:54:54 +0600

    Create session.py

---
+++ session.py
@@ -0,0 +1,11 @@
+import sys
+from telegram import SessionManager
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        api_id = int(sys.argv[1])
+        api_hash = sys.argv[2]
+        phone = sys.argv[3]
+        SessionManager.telethon(api_id, api_hash, phone)
+    else:
+        SessionManager.telethon()
